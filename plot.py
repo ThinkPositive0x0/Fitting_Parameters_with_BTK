3481a73952c900e22299bf1143c7b64e1f0b9959
Xia Chen <chen@localhost.localdomain>
2019-01-20 14:10:25 +0800

    modify Simpson_BTK.c Simpson_BTK.py

--- plot.py
+++ plot.py
@@ -5,8 +5,8 @@ import pandas as pd
 import time
 from btkplot import Dataplot
 time_start=time.time()
-filename1 = 'Nb.csv'
-df2 = pd.read_csv('Nb.csv')
+filename1 = 'b2.csv'
+df2 = pd.read_csv('b2.csv')
 
 # Rename Columns
 def arrange_dataframe(df):
@@ -18,7 +18,7 @@ df2 = arrange_dataframe(df2)
 T = input('Please input Temperature :    (K) \n')
 T = float(T)
 print("Temperature : ",T)
-parameter = [0.731,0.02,0.036,0.0]
+parameter = [0.731,0.08,0.36,0.36]
 time_end=time.time()
 print('Parameters fitting totally cost : ',time_end-time_start)
 Dataplot(parameter,T,df2,'Vdc','G/GN',filename1)
